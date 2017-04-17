fc8d552e5ff9a0a03b033194c771f448ffaf6c46
Sebastián Uriarte Güimil <sebastian.uriarteg@gmail.com>
2017-04-17 14:55:22 -0300

    [Red]: Se crea una nueva prueba unitaria fallida para la property "Name" de la clase "User".

---
+++ Collaborative Board/Unit tests/UserTests.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Unit_tests
+{
+    [TestClass]
+    public class UserTests
+    {
+        private static User testingUser;
+
+        [ClassInitialize]
+        public void TestSetUp()
+        {
+            testingUser = new User();
+        }
+
+        [TestMethod]
+        public void SetValidNameTest()
+        {
+            testingUser.Name = "Mario";
+            Assert.AreEqual("Mario", testingUser.Name);
+        }
+    }
+}
