ee78ef78364207c05d99d798ec00035d2c447a14
Sebastian Delfino <sdelfinoh@gmail.com>
2017-04-22 10:31:59 -0300

    [Red][Team][Name]: Se crean nuevas pruebas unitarias fallidas para la property "Name" de la clase "Team".

---
+++ Collaborative Board/Unit tests/TeamTests.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Unit_tests
+{
+    [TestClass]
+    public class TeamTests
+    {
+        private static Team testingTeam;
+
+        [ClassInitialize]
+        public void TestSetUp()
+        {
+            testingTeam = new Team();
+        }
+
+        private TestContext testContextInstance;
+
+        [TestMethod]
+        public void SetValidNameTest()
+        {
+            testingTeam.Name = "Equipo1";
+            Assert.AreEqual("Equipo1", testingTeam.Name);
+        }
+
+        [TestMethod]
+        public void SetValidNameTest2()
+        {
+            testingTeam.Name = "EquipoNuevo";
+            Assert.AreEqual("EquipoNuevo", testingTeam.Name);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(TeamException))]
+        public void SetInvalidNamePunctuationTest()
+        {
+            testingTeam.Name = "Equipo.#/!";
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(TeamException))]
+        public void SetInvalidNameEmptyTest()
+        {
+            testingTeam.Name = "";
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(TeamException))]
+        public void SetInvalidNameNullTest()
+        {
+            testingTeam.Name = null;
+        }
+    }
+}
