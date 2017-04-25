ffc8d42051b0e910f56619e3c62aa44a8a689c7d
Sebastián Uriarte Güimil <sebastian.uriarteg@gmail.com>
2017-04-25 16:17:42 -0300

    [Red][Comment]: Se crea una serie de pruebas unitarias nuevas vinculadas a los métodos estáticos públicos de creación a generarse en la clase "Comment" y a su property "Creator".

--- Collaborative Board/Unit tests/CommentTests.cs
+++ Collaborative Board/Unit tests/CommentTests.cs
@@ -21,7 +21,53 @@ namespace Unit_tests
         [TestMethod]
         public void CommentForTestingPurposesTest()
         {
+            Assert.IsNull(testingComment.Creator);
             Assert.AreEqual("Comentario inválido.", testingComment.Text);
+        }
+
+        [TestMethod]
+        public void CommentParameterFactoryMethodValidTest1()
+        {
+            string someText = "Falta resolver el issue 12-3.";
+            User creator = User.InstanceForTestingPurposes();
+            testingComment = Comment.CreatorText(creator, someText);
+            Assert.AreEqual(creator, testingComment.Creator);
+            Assert.AreEqual(someText, testingComment.Text);
+        }
+
+        [TestMethod]
+        public void CommentParameterFactoryMethodValidTest2()
+        {
+            string someText = "Falta resolver el issue 12-3.";
+            User creator = User.NamesEmailBirthdatePassword("Emilio", "Ravenna", "ravenna@simuladores.com",
+                DateTime.Now.Date, "contraseñaValida123");
+            testingComment = Comment.CreatorText(creator, someText);
+            Assert.AreEqual(creator, testingComment.Creator);
+            Assert.AreEqual(someText, testingComment.Text);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(CommentException))]
+        public void CommentParameterFactoryMethodInvalidTextTest()
+        {
+            User creator = User.InstanceForTestingPurposes();
+            testingComment = Comment.CreatorText(creator, " \n\n  \t\t \n\t  ");
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(CommentException))]
+        public void CommentParameterFactoryMethodNullTextTest()
+        {
+            User creator = User.InstanceForTestingPurposes();
+            testingComment = Comment.CreatorText(creator, null);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(CommentException))]
+        public void CommentParameterFactoryMethodNullCreatorTest()
+        {
+            string someText = "Falta resolver el issue 12-3.";
+            testingComment = Comment.CreatorText(null, someText);
         }
 
         [TestMethod]
