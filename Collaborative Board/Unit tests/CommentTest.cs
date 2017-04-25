8dda1f3caf7224ac34122508543c22ed8cb6c1bf
Sebastián Uriarte Güimil <sebastian.uriarteg@gmail.com>
2017-04-25 14:36:04 -0300

    [Red][Comment][Text]: Se crean nuevas pruebas unitarias fallidas para la property "Text" de las clase "Comment".

---
+++ Collaborative Board/Unit tests/CommentTest.cs
@@ -0,0 +1,56 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Unit_tests
+{
+    [TestClass]
+    [ExcludeFromCodeCoverage]
+    public class CommentTest
+    {
+        private static Comment testingComment;
+
+        [TestInitialize]
+        public void TestSetUp()
+        {
+            testingComment = CommentTest.InstanceForTestingPurposes();
+        }
+
+        [TestMethod]
+        public void CommentSetTextValid()
+        {
+            string validTextContent = "TODO: Crear la interfaz gráfica.";
+            testingComment.Text = validTextContent;
+            Assert.AreEqual(validTextContent, testingComment.Text);
+        }
+
+        [TestMethod]
+        public void UserSetInvalidFirstNameNumbersTest()
+        {
+            string validTextContent = "123456";
+            testingComment.Text = validTextContent;
+            Assert.AreEqual(validTextContent, testingComment.Text);
+        }
+
+        [TestMethod]
+        public void UserSetInvalidFirstNamePunctuationTest()
+        {
+            string validTextContent = "!@.$#% *-/)(#&^$%^@ !@#";
+            testingComment.Text = validTextContent;
+            Assert.AreEqual(validTextContent, testingComment.Text);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(CommentException))]
+        public void UserSetInvalidLastNameEmptyTest()
+        {
+            testingComment.Text = "";
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(CommentException))]
+        public void UserSetInvalidLastNameNullTest()
+        {
+            testingComment.Text = null;
+        }
+    }
+}
