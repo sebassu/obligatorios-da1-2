5e78e91f1605d889f333e886a6506820ae749981
Sebastian Delfino <sdelfinoh@gmail.com>
2017-04-29 20:00:05 -0300

    [Design][Login]: Se crea la interfaz de usuario para el login.

--- Collaborative Board/Interface/Program.cs
+++ Collaborative Board/Interface/Program.cs
@@ -13,7 +13,7 @@ namespace Interface
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new Login());
         }
     }
 }
