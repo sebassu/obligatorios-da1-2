70b7f1f6d771d32156c479d0a04415016f18da2b
Sebastian Delfino <sdelfinoh@gmail.com>
2017-05-06 17:40:09 -0300

    [Design][WhiteboardVisualization]: Se crea la interfaz de usuario para la visualización de "Pizarrón".

--- Collaborative Board/Interface/WhiteboardVisualization.cs
+++ Collaborative Board/Interface/WhiteboardVisualization.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace Interface
 {
-    public partial class Form1 : Form
+    public partial class WhiteboardVisualization : Form
     {
-        public Form1()
+        public WhiteboardVisualization()
         {
             InitializeComponent();
         }
